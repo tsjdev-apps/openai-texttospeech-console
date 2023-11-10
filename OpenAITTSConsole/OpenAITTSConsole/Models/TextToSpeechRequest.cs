45d6670633404ed8b788e7a0bdee95bb5d408687
Sebastian Jensen <apps@tsjdev-apps.de>
2023-11-10 09:42:57 +0100

    Create TextToSpeechRequest.cs

---
+++ OpenAITTSConsole/OpenAITTSConsole/Models/TextToSpeechRequest.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OpenAITTSConsole.Models;
+
+/// <summary>
+///     Represents a request to generate 
+///     text-to-speech audio.
+/// </summary>
+internal class TextToSpeechRequest
+{
+    /// <summary>
+    ///     The model to use for generating the audio.
+    /// </summary>
+    [JsonPropertyName("model")]
+    public required string Model { get; set; }
+
+    /// <summary>
+    ///     The input text to generate audio for.
+    /// </summary>
+    [JsonPropertyName("input")]
+    public required string Input { get; set; }
+
+    /// <summary>
+    ///     The voice to use for generating the audio.
+    /// </summary>
+    [JsonPropertyName("voice")]
+    public required string Voice { get; set; }
+
+    /// <summary>
+    ///     The format of the response audio.
+    /// </summary>
+    [JsonPropertyName("response_format")]
+    public string ResponseFormat { get; } = "mp3";
+
+    /// <summary>
+    ///     The speed of the generated audio.
+    /// </summary>
+    [JsonPropertyName("speed")]
+    public double Speed { get; } = 1.0;
+}
